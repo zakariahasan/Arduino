4db4318f1d048cc279f656a7b83d5978dca69192
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2019-08-30 22:18:26 +0300

    Update Stepper motor.ino

--- Stepper motor.ino
+++ Stepper motor.ino
@@ -1,16 +1,86 @@
-int stepper_pin_1 = 9;
-int stepper_pin_2 = 10;
-int stepper_pin_3 = 11;
-int stepper_pin_4 = 12;
+int Pin1 = 10; 
+int Pin2 = 11; 
+int Pin3 = 12; 
+int Pin4 = 13; 
+int _step = 0; 
+boolean dir = true;// false=clockwise, true=counter clockwise
+int count=0;
+void setup() 
+{ 
+ pinMode(Pin1, OUTPUT);  
+ pinMode(Pin2, OUTPUT);  
+ pinMode(Pin3, OUTPUT);  
+ pinMode(Pin4, OUTPUT);  
+} 
+ void loop() 
+{ 
+ switch(_step){ 
+   case 0: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, HIGH); 
+   break;  
+   case 1: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, HIGH); 
+     digitalWrite(Pin4, HIGH); 
+   break;  
+   case 2: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, HIGH); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+   case 3: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, HIGH); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+   case 4: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+   case 5: 
+     digitalWrite(Pin1, HIGH);  
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+     case 6: 
+     digitalWrite(Pin1, HIGH);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+   case 7: 
+     digitalWrite(Pin1, HIGH);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, HIGH); 
+   break;  
+   default: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+ } 
+ if(dir){ 
+   _step++; 
+ }else{ 
+   _step--; 
+ } 
+ if(_step>7){ 
+   _step=0; 
+ } 
+ if(_step<0){ 
+   _step=7; 
+ } 
+ delay(1); 
 
-
-int step_number = 0;
-
-
-void setup(){
-pinMode(stepper_pin_1,HIGH);
-pinMode(stepper_pin_2,LOW);
-pinMode(stepper_pin_3,LOW);
-pinMode(stepper_pin_4,LOW);
 }
-void 
