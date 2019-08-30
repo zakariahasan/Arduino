a837d3da16ff1a45258470c33a287169ba1e638d
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2019-08-30 21:38:34 +0300

    Create Stepper motor.ino

---
+++ Stepper motor.ino
@@ -0,0 +1,16 @@
+int stepper_pin_1 = 9;
+int stepper_pin_2 = 10;
+int stepper_pin_3 = 11;
+int stepper_pin_4 = 12;
+
+
+int step_number = 0;
+
+
+void setup(){
+pinMode(stepper_pin_1,HIGH);
+pinMode(stepper_pin_2,LOW);
+pinMode(stepper_pin_3,LOW);
+pinMode(stepper_pin_4,LOW);
+}
+void 
